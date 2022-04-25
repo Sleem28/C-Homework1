942e4bce293066dc5207cac21f6dd1206e4121d9
Kiryl Pashkin <sleembelbox@gmail.com>
2022-04-25 16:33:01 +0300

    add isevennumber

---
+++ IsEvenNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Выяснить является ли число чётным");
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()?? "0");
+
+if(num%2 == 0) Console.WriteLine("Введенное число четное.");
+else           Console.WriteLine("Введенное число нечетное.");
