3a7ed48007ea6893a5e6dd2b24563cdde585a500
Kiryl Pashkin <sleembelbox@gmail.com>
2022-04-27 12:20:56 +0300

    rename all directories

---
+++ 1_FirstSquareSecond/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Проведем проверку, является ли первое введенное число квадратом второго введенного числа.");
+
+Console.Write("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine()?? "0");
+
+Console.Write("Введите второе число: ");
+int secondNum = int.Parse(Console.ReadLine()?? "0");
+
+if(firstNum == secondNum * secondNum)
+{
+    Console.Write($"Число {firstNum} является квадратом числа {secondNum}. ");
+}
+else
+{
+    Console.Write($"Число {firstNum} не является квадратом числа {secondNum}. ");
+}
---
+++ 6_MaxDigitInRandomNumRange/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Дано число из отрезка [10, 99]. Показать наибольшую цифру числа");
+
+int    randomValue  = new Random().Next(10,100);
+string sRandomValue = randomValue.ToString();
+Console.WriteLine("Рандом число равно " + sRandomValue);
+char[] array        = sRandomValue.ToCharArray();
+int    counter      = 0;
+int    maxDigit     = int.MinValue;
+
+while(counter < array.Length) // Можно через for но сделал через While
+{
+    int tmp = int.Parse(array[counter].ToString());
+    if(tmp > maxDigit) maxDigit = tmp;
+    counter++;
+}
+
+Console.WriteLine("Наибольшая цифра рандом числа из отрезка равна " + maxDigit);
