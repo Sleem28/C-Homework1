1c30fd569c6f0cbccac5d86d6f1e7a93170e25bb
Sleem28 <101721768+Sleem28@users.noreply.github.com>
2022-04-27 12:22:55 +0300

    Delete IsEvenNumber directory

--- IsEvenNumber/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Clear();
-Console.WriteLine("Выяснить является ли число чётным");
-
-Console.Write("Введите число: ");
-int num = int.Parse(Console.ReadLine()?? "0");
-
-if(num%2 == 0) Console.WriteLine("Введенное число четное.");
-else           Console.WriteLine("Введенное число нечетное.");
