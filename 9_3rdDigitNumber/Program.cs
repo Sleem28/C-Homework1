0529770b987ee8d95e00c6fa37a1e26fd76d51a9
Kiryl Pashkin <sleembelbox@gmail.com>
2022-04-27 14:57:02 +0300

    modified 9Mathproblem

--- 9_3rdDigitNumber/Program.cs
+++ 9_3rdDigitNumber/Program.cs
@@ -4,65 +4,68 @@ Console.WriteLine("Найти третью цифру числа или сооб
 Console.Write("Введите число: ");
 char[] charNum = (Console.ReadLine() ?? "0").ToCharArray();
 
-if(!TextIsNumbers(ref charNum))
+if(!TextIsNumber(ref charNum))
 {
     Console.WriteLine("В введенном тексте содержатся недопустимые символы!!!");
     return;
 }
 
 if     (charNum[0] == '-' && charNum.Length == 4) Console.WriteLine($"Третья цифра числа равна {charNum[3]}");
-else if(charNum[0] != '-' && charNum.Length == 3) Console.WriteLine($"Третья цифра числа равна {charNum[2]}");
+else if(charNum[0] != '-' && charNum.Length >= 3) Console.WriteLine($"Третья цифра числа равна {charNum[2]}");
 else                                              Console.WriteLine($"Третьей цифры числа НЕТ");
 
 //----------------------------------------Функция проверки введеного текста на наличие любых символов кроме цифр---------------------------------------+
-bool TextIsNumbers(ref char[] array)
+bool TextIsNumber(ref char[] array)
 {
-    bool isNumbers = false;
+    bool isNumber = false;
+    bool found    = false;
     for(int i = 0; i < array.Length; i++)
     {
         switch(array[i])
         {
             case '0': 
-                isNumbers = true;
+                isNumber = true;
                 break;
             
             case '1': 
-                isNumbers = true;
+                isNumber = true;
                 break;
             case '2': 
-                isNumbers = true;
+                isNumber = true;
                 break;
             case '3': 
-                isNumbers = true;
+                isNumber = true;
                 break;
             case '4': 
-                isNumbers = true;
+                isNumber = true;
                 break;
             case '5': 
-                isNumbers = true;
+                isNumber = true;
                 break;
             case '6': 
-                isNumbers = true;
+                isNumber = true;
                 break;
             case '7': 
-                isNumbers = true;
+                isNumber = true;
                 break;
             case '8': 
-                isNumbers = true;
+                isNumber = true;
                 break;
             case '9': 
-                isNumbers = true;
+                isNumber = true;
                 break;
             case '-': 
-                if(i == 0) isNumbers = true; // Проверка первого чара на отрицательное значение
-                else       isNumbers = false;
+                if(i == 0) isNumber = true; // Проверка первого чара на отрицательное значение т.к. число может быть отрицательным
+                else      {isNumber = false; found = true;}
                 break;
             default:
-                isNumbers = false;
+                isNumber = false;
+                found     = true;
                 break;
         }
+        if(found) break;
     }
-    return(isNumbers);
+    return(isNumber);
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------+
