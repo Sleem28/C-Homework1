4b641a97d3652fb73e8152521634d46200b710c1
Kiryl Pashkin <sleembelbox@gmail.com>
2022-04-28 09:22:40 +0300

    change array to string in 9_3rdDigitNumber

--- 9_3rdDigitNumber/Program.cs
+++ 9_3rdDigitNumber/Program.cs
@@ -2,26 +2,26 @@
 Console.WriteLine("Найти третью цифру числа или сообщить, что её нет.");
 
 Console.Write("Введите число: ");
-char[] charNum = (Console.ReadLine() ?? "0").ToCharArray();
+string text = Console.ReadLine() ?? "0";
 
-if(!TextIsNumber(ref charNum))
+if(!TextIsNumber( text))
 {
     Console.WriteLine("В введенном тексте содержатся недопустимые символы!!!");
     return;
 }
 
-if     (charNum[0] == '-' && charNum.Length == 4) Console.WriteLine($"Третья цифра числа равна {charNum[3]}");
-else if(charNum[0] != '-' && charNum.Length >= 3) Console.WriteLine($"Третья цифра числа равна {charNum[2]}");
+if     (text[0] == '-' && text.Length == 4) Console.WriteLine($"Третья цифра числа равна {text[3]}");
+else if(text[0] != '-' && text.Length >= 3) Console.WriteLine($"Третья цифра числа равна {text[2]}");
 else                                              Console.WriteLine($"Третьей цифры числа НЕТ");
 
 //----------------------------------------Функция проверки введеного текста на наличие любых символов кроме цифр---------------------------------------+
-bool TextIsNumber(ref char[] array)
+bool TextIsNumber(string text)
 {
     bool isNumber = false;
     bool found    = false;
-    for(int i = 0; i < array.Length; i++)
+    for(int i = 0; i < text.Length; i++)
     {
-        switch(array[i])
+        switch(text[i])
         {
             case '0': 
                 isNumber = true;
